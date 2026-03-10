cd8dd250e0d9fe294ffb036252fc40a79ff76cdd
karthikeyan <55027856+karthikeyanbaluchamy@users.noreply.github.com>
2026-03-10 10:19:16 -0400

    Refactor variable definitions in variables.tf

--- ere-terraform-src/modules/windows-ec2/variables.tf
+++ ere-terraform-src/modules/windows-ec2/variables.tf
@@ -14,10 +14,6 @@ variable "instance_type" {
   default     = "t3.medium"
 }
 
-variable "subnet_id" {
-  description = "Subnet ID where EC2 will be launched"
-  type        = string
-}
 
 variable "security_group_id" {
   description = "Security Group ID for the EC2 instance"
@@ -40,4 +36,5 @@ variable "instance_count" {
   description = "Number of Windows EC2 instances to create"
   type        = number
   default     = 1
+
 }
