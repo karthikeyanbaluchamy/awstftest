e0ce6ece5cdfceda77e18f9e48525aabd5563aea
karthikeyan <55027856+karthikeyanbaluchamy@users.noreply.github.com>
2026-03-10 10:03:50 -0400

    Add files via upload

---
+++ ere-terraform-src/variables.tf
@@ -0,0 +1,37 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "sg_name" {
+  type = string
+}
+
+variable "sg_description" {
+  type = string
+}
+
+variable "ingress_rules" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
+
+variable "egress_rules" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
+
+variable "tags" {
+  type = map(string)
+}
